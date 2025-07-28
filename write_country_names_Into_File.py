ae72b82b13a28170d7843bb979c8d48c49792855
Vijayasankar v <vij.san05@outlook.com>
2025-07-28 09:32:16 +0530

    write into the file from the you ser choice

--- write_country_names_Into_File.py
+++ write_country_names_Into_File.py
@@ -1,6 +1,10 @@
-with open('country_name.txt','a') as file:
-    countryName = input("Enter the country name")
-    file.write(countryName+'\n')
+while True:
+    with open('country_name.txt','a') as file:
+        countryName = input("Enter the country name")
+        file.write(countryName+'\n')
+        choice= input("Do you wnna continue add name? (y/n)")
+        if choice=='n':
+            break
 
 print("Name has been written")
 
