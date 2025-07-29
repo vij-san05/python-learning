969afb3f293532351960542fdf8c52fbeb1d52b5
Vijayasankar v <vij.san05@outlook.com>
2025-07-29 10:18:45 +0530

    write into the file from the you ser choice added

--- write_country_names_Into_File.py
+++ write_country_names_Into_File.py
@@ -5,6 +5,13 @@ while True:
         choice= input("Do you wnna continue add name? (y/n)")
         if choice=='n':
             break
-
 print("Name has been written")
+
+with open('country_name.txt','r') as file:
+    All_names= file.readlines()
+
+Capital_name =[]
+for name in All_names:
+    Capital_name.append(name.strip().capitalize())
+print(Capital_name)
 
